d713e6b8d54dfde8a135abc845a0902ae6f7c5fc
ikemoti <ikepon413@icloud.com>
2020-02-20 21:23:35 +0900

    基礎的なtableview 設置

--- placelog/ViewController.swift
+++ placelog/ViewController.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController ,UITableViewDataSource,UITableViewDelegate{
+    
+    
+    let myItem :NSArray = ["text1","text2","text3"]
+    
+    @IBOutlet weak var tableview: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableview.delegate =  self
+        tableview.dataSource = self
+        
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myItem.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell",for: indexPath as IndexPath)
+        cell.textLabel?.text = ""
+        return cell
+    }
 
 }
 
